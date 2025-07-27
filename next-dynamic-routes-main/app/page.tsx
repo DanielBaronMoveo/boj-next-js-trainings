cacaa27bebe449f96ca178fd1e94dc21ce964d5b
Daniel Bar-on <danielbo@moveo.co.il>
2025-07-27 14:53:52 +0300

    Day-Trainings

---
+++ next-dynamic-routes-main/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-2xl font-semibold'>Next Dynamic Routes</h1>
+      </div>
+    </section>
+  )
+}
---
+++ next-dynamic-routes-main/next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloudflare-ipfs.com'
+      }
+    ]
+  }
+}
+
+export default nextConfig
